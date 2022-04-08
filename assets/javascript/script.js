8284ce129c7b1fdeed6319294de04301d820930f
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-04-07 17:00:26 -0700

    have basic scaffolding with function to change background color based on time

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,21 @@
 "use strict";
 
-console.log("success");
+document.querySelector("#currentDay").textContent = moment().format('dddd, MMM Do');
+
+function applyColor() {
+    var currentHour = moment().hours();
+    console.log(currentHour, typeof currentHour);
+    for (let i=9; i<18; i++) {
+        if (currentHour > i) {
+            $(`#eventCol${i}`).addClass("past");
+        } else if (currentHour == i) {
+            $(`#eventCol${i}`).addClass("present");
+        } else {
+            $(`#eventCol${i}`).addClass("future");
+        }
+        console.log(i, currentHour, typeof currentHour);
+    }
+}
+
+applyColor();
+
